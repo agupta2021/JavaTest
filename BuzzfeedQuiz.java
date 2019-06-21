90b298735130d39de29aebc52d4cbacfb466094e
Aditya Gupta <30606961+agupta2021@users.noreply.github.com>
2019-06-20 20:47:44 -0700

    Update BuzzfeedQuiz.java

--- BuzzfeedQuiz.java
+++ BuzzfeedQuiz.java
@@ -207,3 +207,71 @@ public class BuzzfeedQuiz {
 		
 	}
 }
+
+////////////////////////////////////
+
+			int bs(int arr[], int l, int r, int x) { 
+			    if (r >= l) { 
+				int mid = l + (r - l) / 2; 
+
+
+				if (arr[mid] == x) 
+				    return mid; 
+
+
+				if (arr[mid] > x) 
+				    return bs(arr, l, mid - 1, x); 
+
+
+				return bs(arr, mid + 1, r, x); 
+			    } 
+
+
+			    return -1; 
+			} 
+
+			int main(void) { 
+			    int arr[] = { 2, 3, 4, 10, 40 }; 
+			    int n = sizeof(arr) / sizeof(arr[0]); 
+			    int x = 10; 
+			    int result = bs(arr, 0, n - 1, x); 
+			    (result == -1) ? printf("Element is not present in array") 
+					   : printf("Element is present at index %d", 
+						    result); 
+			    return 0; 
+			} 
+
+////////////////////
+
+			class Sort { 
+			    void sort(int arr[]) { 
+				int n = arr.length; 
+				for (int i = 0; i < n-1; i++){ 
+				    for (int j = 0; j < n-i-1; j++) {
+					if (arr[j] > arr[j+1]) { 
+					    int temp = arr[j]; 
+					    arr[j] = arr[j+1]; 
+					    arr[j+1] = temp; 
+					} 
+				    }
+				}
+			    } 
+
+			    
+			    void printArray(int arr[]) { 
+				int n = arr.length; 
+				for (int i=0; i<n; ++i){ 
+				    System.out.print(arr[i] + " "); 
+				System.out.println();
+				}
+			    } 
+
+			    
+			    public static void main(String args[]) { 
+				Sort ob = new Sort(); 
+				int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+				ob.sort(arr); 
+				System.out.println("Sorted array"); 
+				ob.printArray(arr); 
+			    } 
+			} 
